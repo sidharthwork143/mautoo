1ef8f5125efdd1d676065777b7e8ecd0fcd50453
RAZi <119275831+MeeRazi@users.noreply.github.com>
2024-12-07 12:37:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 DATABASE_URL = os.environ.get("DATABASE_URL")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "BotsyncBot")
 
 #database
 client = AsyncIOMotorClient(DATABASE_URL)
